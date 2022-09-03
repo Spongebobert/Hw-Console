1b2d3775377fa0e65e8820c14467f75ff6944979
Spongebobert <107082372+Spongebobert@users.noreply.github.com>
2022-09-02 21:52:04 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var first_name = "Ranbir";
+console.log(first_name);
+var last_name = "Kapoor";
+console.log(last_name);
+var a = 10;
+console.log(a);
+var b = 5;
+console.log(b);
+
+var add = a + b ;
+console.log(add);
+var mult = ab ;
+console.log(mult);
+var div = a/b ;
+console.log(div);
+var sub = a - b;
+console.log(sub);
